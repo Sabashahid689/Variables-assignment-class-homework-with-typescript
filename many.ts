4856d1053263717c6db84ca3e6d1636d1562415f
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-06-07 19:20:49 +0500

    Add files via upload

---
+++ many.ts
@@ -0,0 +1,22 @@
+         // class homework assignment with typesccript:-
+                  // topic variables:-
+ // Varriable means data store kerwane ke liye humain varriable ki need hoti hai.
+// There are three types of varriable?
+// (1)var     (2)let     (3)const
+// (1)var is not used that is used for let and const update version ke baad let or const used hote hai
+// var ko hum EF5 version bhi kehte hai.
+// var:-
+//var message = "potato";            //declare
+//message = "tomato";               //update
+//console.log(message);
+//var post = "spinach";            //redeclare
+//console.log(post);
+// let:-
+// let main hum sirf "delcare" or "update" ker sakte hai "redeclare" nhei ker sakte let main.
+//let transport = "car";                //declare
+//transport = "bike";                   //update
+//console.log(transport);
+// const:-
+// const main hum sirf delcare ker sakte hai update or redeclare nhei ker sakte hai.
+//const day = "Thursday";                 //declare
+//console.log(day);                 
